619fe0c90b3109b1bfbd7942b824f7393d6b75fe
Alaa-Hussein002 <ala.hussein002@gmail.com>
2026-07-24 07:14:27 +0300

    feat(vite.config): add vite-plugin-ssr for server-side rendering support

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,13 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import path from 'path'
+import ssr from 'vite-plugin-ssr/plugin';
 
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
+    ssr(),
   ],
   resolve: {
     alias: {
